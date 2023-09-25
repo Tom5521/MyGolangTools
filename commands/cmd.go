4937fae14b0c8204a8e4e8bf80144c970673a3e8
Tom5521 <mismarttv321@gmail.com>
2023-09-25 19:34:35 -0400

    Removing variables with no future and add a without shell function

--- commands/cmd.go
+++ commands/cmd.go
@@ -4,15 +4,17 @@ import (
 	"os"
 	"os/exec"
 	"runtime"
+	"strings"
 )
 
 type Sh struct {
-	RunAsAdmin bool
-	Windows    struct {
+	RunWithShell bool
+	Windows      struct {
 		PowerShell bool // Default terminal/shell is cmd
 	}
 	Linux struct {
-		CustomSh struct {
+		RunWithSudo bool
+		CustomSh    struct {
 			Enable bool
 			ShName string
 			ShArg  string // Shell execution cmd
@@ -29,7 +31,7 @@ type Sh struct {
 
 func (sh Sh) formatCmd() [4]string {
 	var (
-		runas      [2]string
+		sudo       string
 		shell, arg string
 		shells     = [4]string{"sh", "bash", "PowerShell.exe", "cmd"}
 		args       = [2]string{"-c", "/C"}
@@ -42,10 +44,6 @@ func (sh Sh) formatCmd() [4]string {
 			shell = shells[2]
 		} else {
 			shell = shells[3]
-		}
-		if sh.RunAsAdmin {
-			runas[0] = "runas"
-			runas[1] = "/user:administrator"
 		}
 		// Set linux shell formatting
 	} else if current_os == "linux" {
@@ -60,17 +58,23 @@ func (sh Sh) formatCmd() [4]string {
 			shell = sh.Linux.CustomSh.ShName
 			arg = sh.Linux.CustomSh.ShArg
 		}
-		if sh.RunAsAdmin {
-			runas[0] = "sudo"
+		if sh.Linux.RunWithSudo {
+			sudo = "sudo"
 		}
 	}
-	return [4]string{shell, runas[0], runas[1], arg}
+	return [4]string{shell,arg,sudo}
 }
 
 // Exec Cmd method  
 func (sh Sh) Cmd(input string) error {
-	fmtcmd := sh.formatCmd()                                               // Format the command with the respective parameters
-	cmd := exec.Command(fmtcmd[0], fmtcmd[1], fmtcmd[2], fmtcmd[3], input) // declare the *os.Cmd val
+	var cmd *exec.Cmd
+	if !sh.RunWithShell {
+		fmtcmd := sh.formatCmd()                                              // Format the command with the respective parameters
+		cmd = exec.Command(fmtcmd[0], fmtcmd[1], fmtcmd[2], fmtcmd[3], input) // declare the *os.Cmd val
+	} else {
+		input := strings.Fields(input)
+		cmd = exec.Command(input[0], input[1:]...)
+	}
 	// Set the standar input/output/error exit
 	if sh.CustomStd.Enable {
 		if sh.CustomStd.Stdout {
